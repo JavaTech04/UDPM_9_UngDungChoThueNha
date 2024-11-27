581396d747f1e1da87cb0f9d185a45c328f74d7e
Nong Hoang Vu <vunhph33506@fpt.edu.vn>
2024-11-28 00:37:47 +0700

    Change API v3

--- src/api.js
+++ src/api.js
@@ -1,4 +1,2 @@
-// src/api.js
-// export const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiI2MTllYTFmYS1iZGJjLTQ1NzItOGZlMi0zMjk0YmQxM2U2OGUiLCJzdWIiOiJmZDFjMjQ4MC02MjQ3LTQ0NjEtYjlkMi02MmJiNWJhODMzOGQiLCJpYXQiOjE3MzIxODAxMjh9.CN_DHY_v12cvM5Ril93kFsZwERo9YWm3_bFz6A4xzgc";
 export const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiIzYzliMjIwYy02YzZkLTQ4NjktODAwNy02OTEyY2Q0YjBiNjEiLCJzdWIiOiJjNGZlZTI1NC1mYzMxLTRkMjMtYWFmNy1iZGJlOThiNDJmM2QiLCJpYXQiOjE3MzI3MjY4MTN9.WiGH-JyDGtTWXhapajeOq9gdFePVIXtBf0gt-ws40DI";
 export const apiUrl = "https://api.gameshift.dev/nx/users"; // API GameShift Users
